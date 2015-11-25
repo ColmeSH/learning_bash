88381ceac055c748161ff11285e0ed7234dcdac5
blubecks <andreabeccaris88@gmail.com>
2015-11-25 17:30:51 +0100

    Create file with exec permission

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # learning_bash
+
+If you want to add some of these scripts to your path:
+
+* save desired script (without ".sh") in a directory, like ```~/bin```
+* add ```~/bin``` to your ```PATH``` writing ```export PATH=$PATH:~/bin```
+* if you want to make this change persistent, dit ```.bashrc```
---
+++ createFileWithExe.sh
@@ -0,0 +1,30 @@
+#Useful if you want to create a script:
+#	- exec permission
+# 	- bash header
+
+
+#!/bin/bash
+
+read -p "Hi, what is your script's name? " name
+read -p "Would you like to set exec permission for this script? [Y/N] " yn
+if [[ $yn=="Y" || $yn=="y" ]]
+	then
+		read -p "Exec permission for what group? [u/g/o/a] " ugoa
+		touch $name;
+		case "$ugoa" in
+		[Uu])
+			chmod u+x $name
+			;;
+		[Gg])
+			chmod g+x $name
+			;;
+		[Oo])
+			chmod o+x $name
+			;;
+		[Aa])
+			chmod a+x $name
+			;;
+		esac;
+fi
+
+echo -e "#!/bin/bash\n">> $name
